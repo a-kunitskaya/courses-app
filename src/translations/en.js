b7a32ee89c7c8d7b9dcc9f2f3bedb95724b25981
Aleksandra Kunitskaia <alexandra.kunitskaya@gmail.com>
2022-07-09 14:34:25 +0300

    refactor: move translations to separate files

---
+++ src/translations/en.js
@@ -0,0 +1,89 @@
+const en = {
+	registration: {
+		heading: 'Registration',
+		inputName: {
+			name: 'Name',
+			placeholder: 'Enter name',
+		},
+		inputEmail: {
+			name: 'Email',
+			placeholder: 'Enter email',
+		},
+		inputPassword: {
+			name: 'Password',
+			placeholder: 'Enter password',
+		},
+		submitButton: 'Registration',
+		bottomText: 'If you have an account, you can ',
+		bottomTextLink: 'Login',
+		alert: 'Failed to register, reason: ',
+	},
+	login: {
+		heading: 'Login',
+		inputEmail: {
+			name: 'Email',
+			placeholder: 'Enter email',
+		},
+		inputPassword: {
+			name: 'Password',
+			placeholder: 'Enter password',
+		},
+		submitButton: 'Login',
+		bottomText: "If you don't have an account, you can ",
+		bottomTextLink: 'Register',
+		alert: 'Failed to log in, reason: ',
+	},
+	header: {
+		logoutBtnTxt: 'Logout',
+	},
+	user: {
+		defaultUsername: 'Guest',
+	},
+	courseInfo: {
+		authors: 'Authors',
+		created: 'Created',
+		duration: 'Duration',
+		id: 'ID',
+		backToCoursesBtn: 'Back to courses',
+	},
+	courses: {
+		authors: 'Authors',
+		created: 'Created',
+		duration: 'Duration',
+		addNewCourseBtn: 'Add new course',
+		searchInput: {
+			placeholder: 'Enter course name...',
+			submitButton: 'Search',
+		},
+	},
+	createCourse: {
+		courseAuthors: 'Course authors',
+		inputTitle: {
+			name: 'Title',
+			placeholder: 'Enter title...',
+		},
+		createCourseBtn: 'Create course',
+		createAuthorBtn: 'Create author',
+		addAuthorBtn: 'Add author',
+		deleteAuthorBtn: 'Delete author',
+		inputDescription: {
+			name: 'Description',
+			placeholder: 'Enter description',
+		},
+		inputAuthor: {
+			name: 'Author name',
+			placeholder: 'Enter author name',
+		},
+		inputDuration: {
+			name: 'Duration',
+			placeholder: 'Enter duration in minutes',
+		},
+		addAuthorHeading: 'Add author',
+		authorsHeading: 'Authors',
+		durationHeading: 'Duration',
+		courseAuthorsHeading: 'Course authors',
+		noAuthorsTxt: 'Author list is empty',
+	},
+};
+
+export default en;
